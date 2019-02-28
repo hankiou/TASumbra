0da105c31f045da70955f86f7435554d15f001f7
Kappawaii <32612956+Kappawaii@users.noreply.github.com>
2019-02-28 18:49:45 +0100

    Initial commit

---
+++ MainP.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using WindowsInput;
+using WindowsInput.Native;
+
+namespace ConsoleApp1
+{
+    class MainP
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+            InputSimulator i = new InputSimulator();
+            // Simulate each key stroke
+
+            //i.Keyboard.KeyDown(VirtualKeyCode.SHIFT);
+            NOP(3.0);
+            i.Keyboard.KeyDown(VirtualKeyCode.LSHIFT);
+            i.Keyboard.KeyDown(VirtualKeyCode.VK_Z).Sleep(1000).KeyUp(VirtualKeyCode.VK_Z);
+            return;
+            for (int no = 0; no < 13; no++)
+            {
+                i.Mouse.MoveMouseBy(30, 0);
+                NOP(16667);
+            }
+            Console.ReadLine();
+
+        }
+        /// <summary>
+        /// pause pendant n secondes
+        /// </summary>
+        /// <param name="durationSeconds"></param>
+        private static void NOP(double durationSeconds)
+        {
+            var durationTicks = Math.Round(durationSeconds * Stopwatch.Frequency);
+            var sw = Stopwatch.StartNew();
+
+            while (sw.ElapsedTicks < durationTicks)
+            {
+
+            }
+        }
+
+        /// <summary>
+        /// pause pendant n microsecondes
+        /// </summary>
+        /// <param name="microsecs"></param>
+        private static void NOP(int microsecs)
+        {
+            double durationSeconds = ((double)microsecs) / 1000000;
+            var durationTicks = Math.Round(durationSeconds * Stopwatch.Frequency);
+            var sw = Stopwatch.StartNew();
+
+            while (sw.ElapsedTicks < durationTicks)
+            {
+
+            }
+        }
+    }
+}
