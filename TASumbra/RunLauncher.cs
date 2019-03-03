1d7d312067be1908250034637b7cb25a6b078bb1
Kappawaii <32612956+Kappawaii@users.noreply.github.com>
2019-03-04 00:13:35 +0100

    Added Tests tab, more in description
    GameTime tracking
    Frames tracking
    Fps tracking
    Performance meter

--- TASumbra/RunLauncher.cs
+++ TASumbra/RunLauncher.cs
@@ -1,9 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Diagnostics;
 using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace TASumbra
@@ -11,33 +8,88 @@ namespace TASumbra
     class RunLauncher
     {
         private MemoryReader memoryReader;
+        Thread gameClockRefresherThread;
+
+        private Label gameClockLabel;
+        private Label frames;
+        private long framesLong = 0;
+        private Label fps;
+        private int fpsTemp = 0;
+        private Label performanceText;
+
 
         public string penumbraPath;
-        private Label gameClockLabel;
 
-        public RunLauncher(string gamePath,Label gameClockLabel)
+        private void Destroy(object sender, EventArgs e)
         {
-            this.penumbraPath = gamePath;
-            this.memoryReader = new MemoryReader();
+            gameClockRefresherThread.Abort();
+        }
+
+        public RunLauncher(string gamePath,Label gameClockLabel, Label frames, Label fps, Label performanceText)
+        {
+            penumbraPath = gamePath;
+            memoryReader = new MemoryReader();
             this.gameClockLabel = gameClockLabel;
+            this.frames = frames;
+            this.fps = fps;
+            this.performanceText = performanceText;
         }
 
-        public void Start()
+        public bool Start()
         {
-            Thread thread = new Thread(gameClockRefresher);
-            thread.Start();
+            if (!memoryReader.InitPenumbraMemoryReader())
+            {
+                return false;
+            }
+            gameClockRefresherThread = new Thread(GameClockRefresher);
+            Application.ApplicationExit += Destroy;
+            gameClockRefresherThread.Start();
+            return true;
         }
 
-        public void gameClockRefresher()
+        public void GameClockRefresher()
         {
-            rienAFoutre:
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            long stopwatchtime = 0;
+            long fpsCalcTemp = 1;
+            long i = 0;
+            float oldgameClock = 0f;
+            float gameClock;
+            dontCare:
+            //stopwatchtime = stopWatch.ElapsedTicks;
             gameClockLabel.Invoke(new MethodInvoker(delegate
             {
-                gameClockLabel.Text = MemoryReader.ReadPenumbraMemory().ToString();
+                i++;
+                gameClock = memoryReader.GetGameClock();
+                if(gameClock > oldgameClock)
+                {
+                    if(stopwatchtime > fpsCalcTemp*10000000 )
+                    {
+                        FramesPerSecond();
+                        fpsCalcTemp++;
+                    }
+                    oldgameClock = gameClock;
+                    gameClockLabel.Text = gameClock.ToString();
+                    NextFrame();
+                }
+                performanceText.Text = ((stopWatch.ElapsedTicks - stopwatchtime) / 10).ToString() + "µs";
+                stopwatchtime = stopWatch.ElapsedTicks;
             }));
             Thread.Sleep(1);
-            goto rienAFoutre;
+            goto dontCare;
+        }
 
+        public void NextFrame()
+        {
+            frames.Text = (++framesLong).ToString();
+            fpsTemp++;
+        }
+
+        public void FramesPerSecond()
+        {
+            fps.Text = fpsTemp.ToString();
+            fpsTemp = 0;
         }
     }
 }
